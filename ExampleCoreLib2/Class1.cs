f841aa44bf779cebfe88a8afc5e1e43efedb5d3c
lbrealdev <lbrealdeveloper@gmail.com>
2024-11-08 01:11:51 +0100

    feat: update lib and test projects with NUnit unit tests

--- ExampleCoreLib2/Class1.cs
+++ ExampleCoreLib2/Class1.cs
@@ -1,10 +1,28 @@
-﻿namespace ExampleCoreLib2
+﻿using System;
+
+namespace ExampleCoreLib2
 {
-    public class ExampleClass
+    public class MathOperations
     {
         public int Multiply(int a, int b)
         {
             return a * b;
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
         }
     }
 }
--- ExampleCoreLib2Test/Class1.cs
+++ ExampleCoreLib2Test/Class1.cs
@@ -1,15 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using ExampleCoreLib2;
 
 namespace ExampleCoreLib2Test
 {
-    public class ExampleClassTests
+    [TestFixture]
+    public class MathOperationsTests
     {
         [Test]
         public void Multiply_ShouldReturnCorrectResult()
         {
-            var example = new ExampleClass();
-            Assert.AreEqual(6, example.Multiply(2, 3));
+            var mathOps = new MathOperations();
+            int result = mathOps.Multiply(2, 3);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Add_ShouldReturnCorrectResult()
+        {
+            var mathOps = new MathOperations();
+            int result = mathOps.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Subtract_ShouldReturnCorrectResult()
+        {
+            var mathOps = new MathOperations();
+            int result = mathOps.Subtract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Divide_ShouldReturnCorrectResult()
+        {
+            var mathOps = new MathOperations();
+            int result = mathOps.Divide(6, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Divide_ByZero_ShouldThrowDivideByZeroException()
+        {
+            var mathOps = new MathOperations();
+            Assert.Throws<DivideByZeroException>(() => mathOps.Divide(6, 0));
         }
     }
 }
